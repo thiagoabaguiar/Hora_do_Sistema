7ee6bcdcd825d758e2663810d797e80e24369195
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-04-17 11:51:48 -0300

    Initial commit

---
+++ App.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class App{
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora atual do sistema é: " + relogio);
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Obtendo a hora do sistema.
+Curso em Vídeo:
+https://www.youtube.com/watch?v=H7Qpt8NRpDw&list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&index=8
